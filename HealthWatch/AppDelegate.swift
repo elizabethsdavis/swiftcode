a2744a3853a65898b3a981f7c1fe9524c23e3b2d
Elizabeth Davis <elizabethsdavis95@gmail.com>
2015-09-12 14:05:51 -0400

    Initial commit for WatchKit

---
+++ HealthWatch/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  HealthWatch
+//
+//  Created by Elizabeth Davis on 09/12/2015.
+//  Copyright (c) 2015 Electronic Arts, Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+  
+}
+
---
+++ HealthWatchKit/BitWatchKit.h
@@ -0,0 +1,19 @@
+//
+//  HealthWatchKit.h
+//  HealthWatchKit
+//
+//  Created by Elizabeth Davis on 09/12/2015.
+//  Copyright (c) 2015 Electronic Arts, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HealthWatchKit.
+FOUNDATION_EXPORT double HealthWatchKitVersionNumber;
+
+//! Project version string for HealthWatchKit.
+FOUNDATION_EXPORT const unsigned char HealthWatchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HealthWatchKit/PublicHeader.h>
+
+
